90ff9aba93fc1e0d2a611253e58471548ab6b837
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-10 12:34:16 -0400

    Add User and Event models relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :user
+  validates :location, :start_time, :end_time, presence: true
+
+  def overlaps?(other_event)
+    self.start_time < other_event.end_time && other_event.start_time < self.end_time
+  end
 end
