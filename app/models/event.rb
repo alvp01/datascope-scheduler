9374ebd22d000a10e341c4bd6b4db27937fa2a63
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-11 08:05:30 -0400

    Add scopes and validation methods to the Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,15 @@ class Event < ApplicationRecord
   belongs_to :user
   validates :location, :start_time, :end_time, presence: true
 
+  scope :chronological, -> { order(:start_time) }
+
+  scope :today, -> { where(start_time: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
+
   def overlaps?(other_event)
     self.start_time < other_event.end_time && other_event.start_time < self.end_time
+  end
+
+  def lesser_end_time?
+    self.start_time >= self.end_time
   end
 end
