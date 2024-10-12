82b2ea2d115231921197a6fdc85400e6b6e19cbb
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-12 11:48:55 -0400

    Add routes for even creation

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,23 @@ class Api::V1::EventsController < ApplicationController
 
     render json: { events: EventSerializer.new(@events), status: :ok }
   end
+
+  def create
+    user = User.find(params[:user_id])
+
+    render json: { error: "User not found" }, status: :not_found if user.nil?
+
+    begin
+      @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], user).call
+      render json: { event: EventSerializer.new(@event), status: :created }
+    rescue StandardError => e
+      render json: { error: e }, status: :bad_request
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:location, :start_time, :end_time)
+  end
 end
