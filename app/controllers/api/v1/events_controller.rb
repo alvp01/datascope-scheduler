2f0201064f665ec812ef7db0e48f3374f5386c31
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-13 06:59:30 -0400

    Tests for Events controller. events#create

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::EventsController < ApplicationController
       user = User.find(params[:user_id])
 
       @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], user).call
-      render json: { event: EventSerializer.new(@event), status: :created }
+      render json: { event: EventSerializer.new(@event) }, status: :created
     rescue StandardError => e
       render json: { error: e }, status: :bad_request
     end
