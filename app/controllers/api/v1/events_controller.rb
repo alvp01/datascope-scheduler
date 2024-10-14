b5ec480ae16c5f3b91e2d596546b6d7c5290a615
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-14 02:34:50 -0400

    Add EventMailer and added the mailer to the events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,9 +8,10 @@ class Api::V1::EventsController < ApplicationController
   def create
     begin
 
-      user = User.find(params[:user_id])
+      @user = User.find(params[:user_id])
 
-      @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], user).call
+      @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], @user).call
+      EventMailer.event_created(@event, @user).deliver_now
       render json: { event: EventSerializer.new(@event) }, status: :created
     rescue StandardError => e
       render json: { error: e }, status: :bad_request
