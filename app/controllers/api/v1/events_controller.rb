29328d3b9540e30213b2548256a7e3a500738bcb
Abel Lavieri <abel.lavieri@gmail.com>
2024-10-14 05:51:58 -0400

    Update the mailer and controller to reflect the weather change

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -10,9 +10,10 @@ class Api::V1::EventsController < ApplicationController
 
       @user = User.find(params[:user_id])
 
-      @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], @user).call
-      EventMailer.event_created(@event, @user).deliver_now
-      render json: { event: EventSerializer.new(@event) }, status: :created
+      @event = CreateEventService.new(event_params[:location], event_params[:start_time], event_params[:end_time], event_params[:is_physical_location], @user).call
+      @weather = @event.is_physical_location ? WeatherService.new(@event.location).call : nil
+      EventMailer.event_created(@event, @user, @weather).deliver_now
+      render json: { event: EventSerializer.new(@event), weather: @weather }, status: :created
     rescue StandardError => e
       render json: { error: e }, status: :bad_request
     end
@@ -21,6 +22,6 @@ class Api::V1::EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:location, :start_time, :end_time)
+    params.require(:event).permit(:location, :start_time, :end_time, :is_physical_location)
   end
 end
